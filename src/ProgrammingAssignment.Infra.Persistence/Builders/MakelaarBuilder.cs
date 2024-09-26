c2eae3685e8c06bf1ab658e3b41c5df34df1bcc7
Denise Feeleûs <d.feeleus@gmail.com>
2024-09-27 00:03:06 +0200

    Opzetten eerste versie database

---
+++ src/ProgrammingAssignment.Infra.Persistence/Builders/MakelaarBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammingAssignment.Domain.Makelaar;
+
+namespace ProgrammingAssignment.Infra.Persistence.Builders
+{
+    internal class MakelaarBuilder : IEntityTypeConfiguration<Makelaar>
+    {
+        public void Configure(EntityTypeBuilder<Makelaar> builder)
+        {
+            // Todo: fix intern id
+            builder.HasKey(u => u.FundaId);
+        }
+    }
+}
--- src/ProgrammingAssignment.Infra.Persistence/ProgrammingAssignmentContext.cs
+++ src/ProgrammingAssignment.Infra.Persistence/ProgrammingAssignmentContext.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProgrammingAssignment.Domain.Makelaar;
+using System.Reflection;
 
 namespace ProgrammingAssignment.Infra.Persistence;
 
 public class ProgrammingAssignmentContext(DbContextOptions<ProgrammingAssignmentContext> options) : DbContext(options)
 {
     public DbSet<Makelaar> Makelaars { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
