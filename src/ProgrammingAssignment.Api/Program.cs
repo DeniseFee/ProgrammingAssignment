d9679372cc54a0a4d14d776b07295b4f5ac04e8a
Denise Feeleûs <d.feeleus@gmail.com>
2024-09-28 17:43:33 +0200

    Toevoegen ApplicationInsightsTelemitry tbv locale debugging

--- src/ProgrammingAssignment.Api/Program.cs
+++ src/ProgrammingAssignment.Api/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddFundaPartnerApiServices();
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
