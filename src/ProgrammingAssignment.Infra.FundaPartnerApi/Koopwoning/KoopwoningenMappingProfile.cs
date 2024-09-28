03503ea343080e1eb41dee28543c525aceba73d7
Denise Feeleûs <d.feeleus@gmail.com>
2024-09-28 23:22:54 +0200

    Map beschikbaarheid en MakelaarFundaId

--- src/ProgrammingAssignment.Infra.FundaPartnerApi/Koopwoning/KoopwoningenMappingProfile.cs
+++ src/ProgrammingAssignment.Infra.FundaPartnerApi/Koopwoning/KoopwoningenMappingProfile.cs
@@ -7,6 +7,9 @@ public class KoopwoningenMappingProfile : Profile
 {
     public KoopwoningenMappingProfile()
     {
-        CreateMap<Objects, WoningDto>();
+        CreateMap<Objects, WoningDto>()
+            .ForMember(dest => dest.MakelaarFundaId, opt => opt.MapFrom(src => src.MakelaarId))
+            .ForMember(dest => dest.IsTeKoop, opt => opt.MapFrom(src => 
+            !(src.IsVerkocht ?? false) && !(src.IsVerhuurd ?? false) && !(src.IsVerkochtOfVerhuurd ?? false)));
     }
 }
