402346448e2399bacafa172a466f31b266004afd
Denise Feeleûs <d.feeleus@gmail.com>
2024-09-29 10:11:15 +0200

    Toevoegen migrations

---
+++ src/ProgrammingAssignment.Infra.Persistence/Migrations/20240929080218_init.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProgrammingAssignment.Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Makelaars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FundaId = table.Column<int>(type: "int", nullable: false),
+                    Naam = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AantalWoningen = table.Column<int>(type: "int", nullable: false),
+                    TopLijstType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Makelaars", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Makelaars");
+        }
+    }
+}
